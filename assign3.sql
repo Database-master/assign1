34d79a89036002de947b724fb9bf6456e5ca9f39
Cindy Howard <howardcy@lewisu.edu>
2017-08-30 16:34:33 -0500

    added sql files

---
+++ assign3.sql
@@ -0,0 +1,18 @@
+USE skyscrapers;
+-- add sql queries under the appropriate comment
+
+-- exercise 1
+
+-- exercise 2
+
+-- exercise 3
+
+-- exercise 4
+
+-- exercise 5
+
+-- exercise 6
+
+-- exercise 7
+
+-- exercise 8
